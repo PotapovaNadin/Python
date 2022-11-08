726b71d5f082fce57dc3d93c523043dbcfb697a7
Nadezhda <nadin.potapova@icloud.com>
2022-11-08 08:55:08 +0300

    ДЗ 4

--- Desktop/Python/lection/homework/4hw.py
+++ Desktop/Python/lection/homework/4hw.py
@@ -25,6 +25,34 @@
 # Задайте последовательность чисел. 
 # Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
 
+# import random
+# try:
+#     n = int(input('Введите размер списка: '))
+# except:
+#     print('Введите целое число!')
+# list = []
+# def create_list(n):
+#     i = 0
+#     while i < n:
+#         list_number = random.randrange(1, 10)
+#         list.append(list_number)
+#         i +=1
+#     return(list)
+
+# list_of_numbers = create_list(n)
+# print(f'Сформировали список рандомных чисел {list_of_numbers}')
+
+# def only_one_numbers(list):
+#     new_list = []
+#     for i in list:
+#         if i not in new_list:
+#             list_number = i
+#             new_list.append(list_number)
+#     return(new_list)
+
+# list1 = only_one_numbers(list)
+# print(f'Список неповторяющихся элементов {list1}')
+
 # задача 3. 
 # Задана натуральная степень k. 
 # Сформировать случайным образом список коэффициентов
@@ -34,13 +62,43 @@
 
 # - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 
-# задача 4 необязательная. 
-# НА ВХОДЕ ИМЕННО СТРОКА! 
-# Найдите корни квадратного уравнения, уравнение вводит через строку пользователь, 
-# например, 6*x^2+5*x+6=0 . Само собой, уравнение может и не иметь решения. 
-# Предусмотреть все варианты, сделать обработку исключений.
+# import random
 
-# задача 5 необязательная 
-# Сделать локальный чат-бот с JSON хранилищем на основе приложенного буткемпа. 
-# Тема чат-бота любая. Просьба - постараться не использовать простой одномерный список 
-# или простой одномерный словарь.
+# def write_file(st):
+#     with open('file4.txt', 'w') as data:
+#         data.write(st)
+
+
+# def rnd():
+#     return random.randint(0,101)
+
+
+# def create_mn(k):
+#     lst = [rnd() for i in range(k+1)]
+#     return lst
+    
+
+# def create_str(sp):
+#     lst= sp[::-1]
+#     wr = ''
+#     if len(lst) < 1:
+#         wr = 'x = 0'
+#     else:
+#         for i in range(len(lst)):
+#             if i != len(lst) - 1 and lst[i] != 0 and i != len(lst) - 2:
+#                 wr += f'{lst[i]}x^{len(lst)-i-1}'
+#                 if lst[i+1] != 0:
+#                     wr += ' + '
+#             elif i == len(lst) - 2 and lst[i] != 0:
+#                 wr += f'{lst[i]}x'
+#                 if lst[i+1] != 0:
+#                     wr += ' + '
+#             elif i == len(lst) - 1 and lst[i] != 0:
+#                 wr += f'{lst[i]} = 0'
+#             elif i == len(lst) - 1 and lst[i] == 0:
+#                 wr += ' = 0'
+#     return wr
+
+# k = int(input("Введите натуральную степень k = "))
+# koef = create_mn(k)
+# write_file(create_str(koef))
